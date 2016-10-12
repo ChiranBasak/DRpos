63656e56c874476347cf24c217a7bf875ee24968
Chiranjeeb <chiranjeeb.basak@sca-solutions.com>
2016-10-12 09:18:42 +0200

    My First Commit

---
+++ BankAccount/BankAccount/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary> 
+    /// Bank Account demo class. 
+    /// </summary> 
+    public class BankAccount
+    {
+        public enum accountType
+        {
+            Gold,
+            Platinum,
+        }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public accountType GetAccountType
+        {
+            get
+            {
+                if (Balance > 15.00)
+                    return accountType.Platinum;
+                else
+                    return accountType.Gold;
+            }
+        }
+        private string m_customerName;
+        private double m_balance;
+    }
+}
---
+++ BankAccount/BankAccountTests/BankAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void GetAccountTypeTest()
+        {
+            string customerName = "Mr. Bryan Walton";
+            double balance = 15.01;
+            BankAccount target = new BankAccount(customerName, balance);
+            BankAccount.accountType actual;
+            actual = target.GetAccountType;
+            Assert.AreEqual(actual, BankAccount.accountType.Platinum);
+        }
+    }
+}
